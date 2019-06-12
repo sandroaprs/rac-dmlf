71d2d7ed53e3e0e77d3f6f0fe52a427834f932c6
sandroaprs <48067356+sandroaprs@users.noreply.github.com>
2019-06-12 16:25:18 -0300

    Commit inicial do projeto

---
+++ src/br/gov/ipem/racdmlf/model/Funcionario.java
@@ -0,0 +1,29 @@
+package br.gov.ipem.racdmlf.model;
+
+public class Funcionario {
+	
+	private Integer codigo;
+	private String nome;
+	
+	
+	public Funcionario(Integer codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
